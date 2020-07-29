b1b8c7ff698f7e80f98a8ccc3eb72ef19b9de894
吴震宇 <wu.zhenyu@darcytech.com>
2020-07-29 13:02:41 +0800

    bugfix

--- types/api/device/network.d.ts
+++ types/api/device/network.d.ts
@@ -183,29 +183,86 @@ declare namespace my {
    */
   function offNetworkStatusChange(): void;
 
-  interface IHttpRequestOptions {
+  interface IHttpRequestSuccessResult {
     /**
-     * HTTP接口路径
+     * 响应数据，格式取决于请求时的 dataType 参数
      */
-    path: string;
+    readonly data?: any;
+
     /**
-     * GET,POST。必须是大写
+     * 响应码
+     *
+     * - 11：无权跨域
+     * - 12：网络出错
+     * - 13：超时
+     * - 14：解码失败
+     * - 15：HTTP错误
+     * - 16：请求已被停止/服务端限流
      */
-    method?: 'GET' | 'POST';
+    readonly status?: 11 | 12 | 13 | 14 | 19;
+
     /**
-     * URL参数
+     * 响应头
      */
-    params: Object;
-    /**
-     * 放置在BODY中的参数
-     */
-    body?: Object;
-    /**
-     * HTTP请求头参数
-     */
-    headers?: Object;
-    exts?: Object;
+    readonly headers?: Readonly<Record<string, string>>;
   }
 
-  function httpRequest(options: IHttpRequestOptions): any;
+  interface IHttpRequestOptions {
+    /**
+     * 目标服务器url
+     */
+    url: string;
+
+    /**
+     * 设置请求的 HTTP 头，默认 {'content-type': 'application/json'}
+     */
+    headers?: Record<string, string>;
+
+    /**
+     * 默认GET，目前支持GET/POST
+     */
+    method?: 'GET' | 'POST';
+
+    /**
+     * 请求参数。
+     *
+     * 传给服务器的数据最终会是 String 类型，如果 data 不是 String 类型，会被转换成 String 。转换规则如下：
+     * - 若方法为GET，会将数据转换成 query string： encodeURIComponent(k)=encodeURIComponent(v)&encodeURIComponent(k)=encodeURIComponent(v)...
+     * - 若方法为 POST 且 headers['content-type'] 为 application/json ，会对数据进行 JSON 序列化
+     * - 若方法为 POST 且 headers['content-type'] 为 application/x-www-form-urlencoded ，会将数据转换成 query string： encodeURIComponent(k)=encodeURIComponent(v)&encodeURIComponent(k)=encodeURIComponent(v)...
+     */
+    data?: Record<string, any>;
+
+    /**
+     * 超时时间，单位ms，默认30000
+     */
+    timeout?: number;
+
+    /**
+     * 期望返回的数据格式，默认json，支持json，text，base64
+     */
+    dataType?: 'json' | 'text' | 'base64';
+
+    /**
+     * 调用成功的回调函数
+     */
+    success?(res: IHttpRequestSuccessResult): void;
+
+    /**
+     * 调用失败的回调函数
+     */
+    fail?(res: any): void;
+
+    /**
+     * 调用结束的回调函数（调用成功、失败都会执行）
+     */
+    complete?(res: any): void;
+  }
+
+  /**
+   * 小程序网络请求
+   *
+   * @deprecated
+   */
+  function httpRequest(options: IHttpRequestOptions): void;
 }
