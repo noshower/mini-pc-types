8c381a17b7cad6162a93a59b998bc204a6dd4d62
吴震宇 <wu.zhenyu@darcytech.com>
2020-08-14 19:46:48 +0800

    feat 增加createSelectorQuery

--- types/api/index.d.ts
+++ types/api/index.d.ts
@@ -5,3 +5,31 @@
 /// <reference path="./media/index.d.ts" />
 /// <reference path="./open/index.d.ts" />
 /// <reference path="./ui/index.d.ts" />
+
+declare namespace my {
+  interface IBoundingClientRect {
+    width: number;
+    height: number;
+    top?: number;
+    right?: number;
+    bottom?: number;
+    left?: number;
+  }
+
+  type ScrollOffset = Record<'scrollTop' | 'scrollLeft', number>;
+
+  type SelectorResult = ReadonlyArray<null | Readonly<IBoundingClientRect> | Readonly<ScrollOffset>>;
+
+  type ISelectorExecCallback = (ret: SelectorResult) => void;
+
+  interface ISelectorQuery {
+    select(selector: string): ISelectorQuery;
+    selectAll(selector: string): ISelectorQuery;
+    selectViewport(): ISelectorQuery;
+    boundingClientRect(): ISelectorQuery;
+    scrollOffset(): ISelectorQuery;
+    exec(ret: ISelectorExecCallback): void;
+  }
+
+  function createSelectorQuery(params?: Record<string, any>): ISelectorQuery;
+}
